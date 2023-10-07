94688d6ac63e757cd71104371d2d1865e3b33911
CLARISSA CHEN <clarissachen5@gmail.com>
2023-10-07 15:36:58 -0400

    added button ids and started click functionality

---
+++ script.js
@@ -0,0 +1,7 @@
+var button = document.getElementById("redirectButton");
+
+        // Add a click event listener to the button
+        document.getElementById("OrderForDelivery").addEventListener("click", function() {
+            // Redirect the user to a different page
+            window.location.href = "order.html"; // Replace with the URL you want to redirect to
+        });
