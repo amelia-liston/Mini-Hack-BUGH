8a445be4097c595308a773fa87a9b4d5d6ed95cd
CLARISSA CHEN <clarissachen5@gmail.com>
2023-10-07 15:44:14 -0400

    fixed buttons

--- script.js
+++
@@ -1,7 +0,0 @@
-var button = document.getElementById("redirectButton");
-
-        // Add a click event listener to the button
-        document.getElementById("OrderForDelivery").addEventListener("click", function() {
-            // Redirect the user to a different page
-            window.location.href = "order.html"; // Replace with the URL you want to redirect to
-        });
